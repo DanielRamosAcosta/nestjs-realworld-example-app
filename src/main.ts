5855bf10b5e4c7d822e83a4c03248e0254d53e09
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-05 10:49:39 +0200

    rename to ci

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-// trigger ci
-
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
